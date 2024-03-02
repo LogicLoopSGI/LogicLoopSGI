03fdfdc9d3d2b4a07de26d47b80463326a46c186
Atharv-28 <atharvmt@outlook.com>
2024-03-02 17:42:08 +0530

    Mobile version beta ready

--- Scripts/sidePanel.js
+++ Scripts/sidePanel.js
@@ -1,4 +1,4 @@
 function toggleNav() {
     var navRight = document.querySelector('.nav-right');
-    navRight.style.display = (navRight.style.display === 'none' || navRight.style.display === '') ? 'block' : 'none';
+    navRight.style.display = navRight.style.display === 'none' || navRight.style.display === '' ? 'block' : 'none';
 }
