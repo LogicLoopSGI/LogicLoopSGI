41377a331b785a748d1efc80edec27cf86d5ae38
Atharv-28 <atharvmt@outlook.com>
2024-03-02 17:07:46 +0530

    Working on navbar

---
+++ Scripts/sidePanel.js
@@ -0,0 +1,4 @@
+function toggleNav() {
+    var navLeft = document.querySelector('.nav-left');
+    navLeft.style.display = (navLeft.style.display === 'none' || navLeft.style.display === '') ? 'block' : 'none';
+}
