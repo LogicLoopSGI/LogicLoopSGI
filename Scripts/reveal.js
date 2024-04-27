9cc7661dfd63a01bd7dd78e5271b778dd8903626
Atharv-28 <atharvmt@outlook.com>
2024-04-27 22:19:31 +0530

    animation added & reveal added to 4 pages

--- Scripts/reveal.js
+++ Scripts/reveal.js
@@ -1,2 +1,13 @@
 ScrollReveal().reveal('.eventCard',{delay:500, reset: true, easing: 'ease-in'});
 ScrollReveal().reveal('.wsCard',{delay: 1000, reset: true, easing: 'ease-in'});
+
+
+ScrollReveal().reveal('.eventBox',{delay: 500, reset: true, easing: 'ease-in'});
+ScrollReveal().reveal('#eventHeading',{delay: 1000, reset: true, easing: 'ease-in'});
+ScrollReveal().reveal('.imageSlider',{delay: 1300, reset: true, easing: 'ease-in'});
+ScrollReveal().reveal('.eventDetails',{delay: 500, reset: true, easing: 'ease-in'});
+
+
+ScrollReveal().reveal('.department-heading',{delay: 200, reset: true, easing: 'ease-in'});
+ScrollReveal().reveal('.department-members',{delay: 400, reset: true, easing: 'ease-in'});
+ScrollReveal().reveal('.team-card',{delay: 450, reset: true, easing: 'ease-in'});
